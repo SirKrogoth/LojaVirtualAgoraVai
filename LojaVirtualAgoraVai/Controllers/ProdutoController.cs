a4bbccaf1e590cecde566895212d59b5cc6df536
João Rafael Fagundes Menezes <menezes.jrafael@gmail.com>
2020-03-23 00:57:36 -0300

    Finalizando Sessão 07

---
+++ LojaVirtualAgoraVai/Controllers/ProdutoController.cs
@@ -0,0 +1,32 @@
+﻿using LojaVirtualAgoraVai.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtualAgoraVai.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Visualizar()
+        {
+            Produto produto = GetProduto();
+
+            return View(produto);
+
+            //return new ContentResult() { Content = "<h3>Loja Agora vai . com</h3>", ContentType="text/html"};
+        }
+
+        public Produto GetProduto()
+        {
+            return new Produto()
+            {
+                Id = 1,
+                Nome = "Caneca personalizada",
+                Descricao = "Caneca full hd",
+                Valor = 15.99
+            };
+        }
+    }
+}
